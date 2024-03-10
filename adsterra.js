142f36bc677db8ff10ccbd0850b3484a977b76d3
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-03-10 14:42:22 +0700

    Create adsterra.js

---
+++ adsterra.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'ad5109b9b5c7e2217f65e849fda5d903',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/ad5109b9b5c7e2217f65e849fda5d903/invoke.js"></scr' + 'ipt>');
